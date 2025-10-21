02b55fe25aff4ce8cb4810eac0b8d3275222f778
Anirudh Sharma <48174424+anirudhsharma-2000@users.noreply.github.com>
2025-10-21 22:35:56 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,14 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    // Important for serverless:
+    maxPoolSize: 10,
+    minPoolSize: 2,
+    maxIdleTimeMS: 10000,
+  });
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
